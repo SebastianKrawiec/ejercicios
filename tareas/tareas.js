f2174cc448cde0ed645b32146fff387c48887597
SebastianKrawiec <59486888+SebastianKrawiec@users.noreply.github.com>
2020-11-23 20:57:52 -0300

    Update tareas.js
    borrando comentarios

--- tareas/tareas.js
+++ tareas/tareas.js
@@ -11,17 +11,3 @@ module.exports = moduloTareas = {
         //esto lo parsea para poder trabajarlo
     }
 }
-/* conviene tenerlo así para poder saber con qué
-está trabajando. a su vez ya queda exportado*/
-
-
-/* En esta ejercitación te pediremos que desarrolles una pequeña aplicación modularizada que,
-dado un archivo .json de productos, al ejecutarla por línea de comandos pueda recibir las
-siguientes instrucciones:
-- listar todos los productos
-- agregar un nuevo producto
-- filtrar los productos según su precio(ej.: Todos los productos que tengan un precio entre 20 y 100)
-- modificar el precio de un producto específico
-+Extras+
-- eliminar un producto
-- buscar un producto */
